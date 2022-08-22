8e859af8f6954da50db4fecf0452fff400e890a7
Azi Hassan <azi.hassan@live.fr>
2022-08-22 22:52:22 +0100

    [master] Load todos from jsonplaceholder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'csr-example';
+  todos: Todo[] = [];
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  ngOnInit() {
+    this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos').subscribe(
+      (success) => {
+        this.todos = success;
+      },
+      (error) => {
+        console.log("Failed to load todos : ", error);
+      }
+    );
+  }
+}
+
+interface Todo {
+  id: number;
+  userId: number;
+  title: string;
+  completed: boolean;
 }
